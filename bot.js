fe8d7e75f2a8e24fb386bd7a10dd23637ad8c39d
Osama2001 <40809896+Osama2001@users.noreply.github.com>
2018-07-21 14:14:46 +0300

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const bot = new Discord.Client();
+
+const prefix = "-";
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log('By Osama_DK')
+  console.log('')  
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
+  console.log('╚[════════════════════════════════════]╝')
+});
+
+
+
+
+
+
+console.log("Welcome Again !");
+ 
+ client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+https://discord.gg/hKtvV9T
+ ${member}  
+**`)
+}).catch(console.error)
+
+})
+ 
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+https://discord.gg/hKtvV9T
+ ${member}  
+**`) 
+}).catch(console.error)
+})
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
